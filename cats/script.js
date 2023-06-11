99b4788e208401528b7c2b1a5e00bcfc82a02ecc
gabrazio <gabrielebuonocore.gb@gmail.com>
2023-06-12 00:13:14 +0200

    -

--- cats/script.js
+++ cats/script.js
@@ -1,12 +1,14 @@
 window.onload = () => {
+    var header = document.getElementById("title");
     var pageNumber = 0;
+    var actualScroll = 0;
+    var oldScroll = 0;
 
     function httpRequest(number, limit){
         let url = 'https://api.thecatapi.com/v1/images/search?limit=' + limit + '&page=' + number + '&api_key=live_tVsqwwsPPdgBurscYsbyIYVW1bKzMti9drm9cKp2jmhirNd7El0BL8ykdzSZBPd0';
         fetch(url).then((res) => {
             return res.json();
         }).then((data) => {
-            console.log(data);
             for(let i in data){
             const img = document.createElement("img");
             img.setAttribute("src", String(data[i].url));
@@ -18,9 +20,20 @@ window.onload = () => {
     httpRequest(pageNumber, 10);
 
     window.addEventListener('scroll', (event) => {
-        if ((window.innerHeight + Math.ceil(window.pageYOffset)) >= document.body.offsetHeight) {
+        actualScroll = $(window).scrollTop();
+        
+        if(actualScroll > oldScroll){
+            header.className = "hide-header";
+            
+        } else {
+            header.className = "show-header";
+        }
+
+        oldScroll = actualScroll;
+
+        if(($(window).scrollTop()) > ($(document).height() / 2)){
             pageNumber++;
             httpRequest(pageNumber, 100);
         }
-    });                
+    });              
 }
