7ab8345d6632777b616530cffa05358372b147af
gabrazio <gabrielebuonocore.gb@gmail.com>
2023-06-12 20:54:22 +0200

    -

--- cats/script.js
+++ cats/script.js
@@ -3,14 +3,17 @@ window.onload = () => {
     var pageNumber = 0;
     var actualScroll = 0;
     var oldScroll = 0;
-    var halfScroll = false;
-    var onRequest = false;
     var firstRequest = true;
-    var stopRequest = false;
+    var onRequest = false;
 
-    function httpRequest(number, limit) {
+    setInterval(() => {
+        onRequest = false;
+    }, 5000)
+
+    async function httpRequest(number, limit) {
+        onRequest = true;
         let url = 'https://api.thecatapi.com/v1/images/search?limit=' + limit + '&page=' + number + '&api_key=live_tVsqwwsPPdgBurscYsbyIYVW1bKzMti9drm9cKp2jmhirNd7El0BL8ykdzSZBPd0';
-        fetch(url).then((res) => {
+        await fetch(url).then((res) => {
             return res.json();
         }).then((data) => {
             for (let i in data) {
@@ -24,35 +27,27 @@ window.onload = () => {
                 location.reload(true);
             }
         });
-        onRequest = false;
-        halfScroll = false;
     }
 
     httpRequest(pageNumber, 10);
     firstRequest = false;
 
-    if(stopRequest == false){
-        window.addEventListener('scroll', (event) => {
-            actualScroll = $(window).scrollTop();
+    window.addEventListener('scroll', (event) => {
+        actualScroll = $(window).scrollTop();
 
-            if (actualScroll > oldScroll) {
-                header.className = "hide-header";
+        if (actualScroll > oldScroll) {
+            header.className = "hide-header";
 
-            } else {
-                header.className = "show-header";
-                halfScroll = false;
-            }
+        } else {
+            header.className = "show-header";
+            halfScroll = false;
+        }
 
-            oldScroll = actualScroll;
-
-            if (($(window).scrollTop()) > ($(document).height() / 2)) {
-                halfScroll = true;
-            }
-
-            if ((halfScroll == true) && onRequest == false){
-                pageNumber++;
-                httpRequest(pageNumber, 100);
-            }
-        });
-    }
+        oldScroll = actualScroll;
+        
+        if (($(window).scrollTop()) > ($(document).height() / 2) && (onRequest == false)) {
+            pageNumber++;
+            httpRequest(pageNumber, 100);
+        }
+    });
 }
