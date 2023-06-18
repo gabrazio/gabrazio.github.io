c666a50cbb51ff82f20a718d8bb52ba62f1dc47a
gabrazio <gabrielebuonocore.gb@gmail.com>
2023-06-18 16:37:35 +0200

    -

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 window.onload = () => {
     // @grant        window.close
-
+    
+    // Generic Variables
+    var windowClicked = false;
     const music = [];
+    var texts = ["Hello World!", "AaAaaAaH!!!", "Drink some water! ;)", "WhaAaT?! D:", "Welcome!! :)"];
+
+    // Audio
+    const audio = new Audio('assets/audio/click.mp3');
     music.push(new Audio('assets/audio/Moog City.mp3'));
     music.push(new Audio('assets/audio/Minecraft.mp3'));
-    var windowClicked = false;
+
+    // DOM Elements
+    const singleplayer = document.getElementById("singleplayer");
+    const mutiplayer = document.getElementById("multiplayer");
+    const options = document.getElementById("options");
+    const quit = document.getElementById("quit");
 
     function resetPage(){
         document.body.innerHTML = `<div id="container">
@@ -23,21 +34,12 @@ window.onload = () => {
                                 </div>`;
     }
 
-    const singleplayer = document.getElementById("singleplayer");
-    const mutiplayer = document.getElementById("multiplayer");
-    const options = document.getElementById("options");
-    const quit = document.getElementById("quit");
-    
-    const audio = new Audio('assets/audio/click.mp3');
-
     window.addEventListener("click", () => {
         if(windowClicked == false){
             windowClicked = true;
             music[Math.floor(Math.random()*music.length)].play();
         }
     });
-
-    var texts = ["Hello World!", "AaAaaAaH!!!", "Drink some water! ;)", "WhaAaT?! D:", "Welcome!! :)"];
 
     document.getElementById("text").innerHTML = String(texts[Math.floor(Math.random()*texts.length)]);
 
